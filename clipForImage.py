73787fb7cd4fa8f739a95c3f2c8e02bf9a21be99
Margaret910302 <79899901+Margaret910302@users.noreply.github.com>
2023-03-05 22:20:53 +0800

    影片轉圖片

---
+++ clipForImage.py
@@ -0,0 +1,19 @@
+import cv2
+import math
+
+cap = cv2.VideoCapture('video//江八點健身操10-雙手托天.mp4')
+frameRate = cap.get(5) #frame rate, every 5 frame 
+print(frameRate)
+while(cap.isOpened()):
+    frameId = cap.get(1) #current frame number
+    print(frameId)
+    ret, frame = cap.read()
+    if (ret != True):
+        break
+    #if (frameId % math.floor(frameRate) == 0):
+    if (frameId % math.floor(5) == 0):
+        filename = "image_" +  str(int(frameId)) + ".jpg"
+        filename = "imgs//" + filename
+        cv2.imwrite(filename, frame)
+cap.release()
+print("Done!")
